627608900deee6c02ef21c56f221e1ff3dc139b4
duynhannguyen <130212806+duynhannguyen@users.noreply.github.com>
2023-07-21 00:14:22 +0700

    update

--- learn-http/src/App.js
+++ learn-http/src/App.js
@@ -1,24 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import Homepage from './pages/Homepage/Homepage';
+import LoginPage from './pages/LoginPage/LoginPage';
+import Signup from './pages/Signup/Signup';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Routes>
+      <Route path='/' element={<Homepage/>}/>
+      <Route path='/login' element={<LoginPage/>}/>
+      <Route path='/signup' element={<Signup/>}/>
+    </Routes>
     </div>
+    </BrowserRouter>
   );
 }
 
